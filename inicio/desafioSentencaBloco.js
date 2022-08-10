bc1fb08c2ba4e5f1beb096d79002405780b62ca6
Victor Pinas Arnault <victor.arnault@yahoo.com>
2022-08-10 13:06:26 -0300

    Desafio de sentença de blocos

---
+++ inicio/desafioSentencaBloco.js
@@ -0,0 +1,14 @@
+/*Exiba no console, os números pares e ímpares de 1 a 10 
+* Faça um bloco para cada.
+* Um bloco para os números ímpares, outro para os números pares
+*/
+
+//Bloco para os ímpares
+{
+    console.log(1, 3, 5, 7, 9);
+}
+
+//Bloco para os pares
+{
+    console.log(2, 4, 6, 8, 10);
+}
