2a8f9e3ad5095d2d5cd4f4119c5d836c2ff0f696
Victor Pinas Arnault <victor.arnault@yahoo.com>
2023-02-16 11:32:45 -0300

    adicionando os outros programas

---
+++ inicio/dados_03.js
@@ -0,0 +1,11 @@
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4578.29;
+console.log(typeof salario);
+
+let estaChovendo = true;
+console.log(typeof estaChovendo);
+
+console.log(typeof "Teste");
